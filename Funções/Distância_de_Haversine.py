824a1c96d11e41f04cc96661edc1c8b41355d7fc
caiostabile <103457803+caiostabile@users.noreply.github.com>
2022-05-13 11:00:36 -0300

    Update Distância_de_Haversine.py

--- Funções/Distância_de_Haversine.py
+++ Funções/Distância_de_Haversine.py
@@ -4,3 +4,5 @@ def haversine(r,l1,f1,l2,f2):
     parte2=math.cos(math.radians(l1))* math.cos(math.radians(l2))*(math.sin(math.radians((f2-f1)/2)))**2
     resposta = 2*r*math.asin((parte1+parte2)**0.5)
     return resposta
+
+Caio
