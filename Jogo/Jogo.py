7f6067f80e456b13cd174398515fd0e2ba4df6fb
Henrique <henriquecrc@al.insper.edu.br>
2022-05-13 12:17:05 -0300

    função no jogo

--- Jogo/Jogo.py
+++ Jogo/Jogo.py
@@ -1,2 +1,14 @@
-print("JOGO DOS PAÍSES")
+print("JOGO DOS PAÍSES\nComandos:\ndica - entra no mercado de dicas")
+
+
+import random
+def sorteia_pais(dados):
+    lista_paises = []
+    for pais in dados:
+        lista_paises.append(pais)
+    pais_sorteado= random.choice(lista_paises)
+    return pais_sorteado
+
+pais_escolhido = sorteia_pais
+
 
