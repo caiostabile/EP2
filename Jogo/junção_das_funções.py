47c29e29fa51d5f104358e4ac04670ff6e8b7ed8
caiostabile <103457803+caiostabile@users.noreply.github.com>
2022-05-13 11:32:12 -0300

    Update junção_das_funções.py

--- Jogo/junção_das_funções.py
+++ Jogo/junção_das_funções.py
@@ -6,3 +6,21 @@ def normaliza(dic):
             dic_vazio[pais]=dados
             dic_vazio[pais]['continente']=cont
     return dic_vazio
+
+### Função 2
+import random
+def sorteia_pais(dados):
+    lista_paises = []
+    for pais in dados:
+        lista_paises.append(pais)
+    resultado = random.choice(lista_paises)
+    return resultado
+
+### Função 3
+
+### Função 4
+
+### Função 5
+
+### Função 6
+
