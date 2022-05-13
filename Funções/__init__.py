63a3ec4a641f0b4c2e2693f37167f7e592f7ceec
Henrique <henriquecrc@al.insper.edu.br>
2022-05-13 09:53:22 -0300

    Função 5

---
+++ Funções/__init__.py
@@ -0,0 +1,13 @@
+#Funções:
+
+
+#F5 :
+def esta_na_lista(nome_pais, lista_paises):
+    i = 0
+    for sublista in lista_paises:
+        if nome_pais in sublista:
+            i += 1    
+    if i > 0:
+        return True
+    else:
+        return False 
