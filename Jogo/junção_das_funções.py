cd63a420f48c85fbe735eae694eb83255344b91e
caiostabile <103457803+caiostabile@users.noreply.github.com>
2022-05-13 11:29:55 -0300

    Update junção_das_funções.py

---
+++ Jogo/junção_das_funções.py
@@ -0,0 +1,8 @@
+### Função 1
+def normaliza(dic):
+    dic_vazio={}
+    for cont,pais in dic.items():
+        for pais,dados in pais.items():
+            dic_vazio[pais]=dados
+            dic_vazio[pais]['continente']=cont
+    return dic_vazio
