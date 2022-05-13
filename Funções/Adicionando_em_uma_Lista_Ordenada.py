551680c70bef10a9ca383efd15572db57c413436
Henrique <henriquecrc@al.insper.edu.br>
2022-05-13 10:34:29 -0300

    função 4

---
+++ Funções/Adicionando_em_uma_Lista_Ordenada.py
@@ -0,0 +1,25 @@
+def adiciona_em_ordem(nome_pais, distancia, lista_paises):
+
+    adicionar = [nome_pais,distancia]
+    i = 0
+    if len(lista_paises) > 0 :
+        tamanho = len(lista_paises)
+        ultimo_ele = lista_paises[tamanho-1]   
+        distancia_ultimo_elemnto = ultimo_ele[1]
+    if len(lista_paises) == 0:
+        resposta = lista_paises.append(adicionar)
+        
+    for sublista in lista_paises:
+        if sublista[0] == nome_pais:
+            return lista_paises
+            
+        if sublista[0] != nome_pais:
+            if sublista[1] >= distancia:
+                lista_paises.insert(i,adicionar)
+                return lista_paises
+            if distancia > distancia_ultimo_elemnto:
+                lista_paises.append(adicionar)
+                return lista_paises
+                        
+        i += 1
+print(adiciona_em_ordem('chile',18000,[['libia',3678],['franca',3998],['egito',5008],['siria',5919],['india',9919],['japao',13836]]))
