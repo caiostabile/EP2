4c6bac92cf18f2cd022eb81d2f736f9ebcd6149c
Henrique <henriquecrc@al.insper.edu.br>
2022-05-13 16:27:24 -0300

    calcula distancia

--- Jogo/Jogo.py
+++ Jogo/Jogo.py
@@ -7,15 +7,35 @@ from junção_das_funções import *
 from Dados import *
 
 #variaveis:
-i = 0 
+i = 0
+lista_paises = []
 
 #funções do jogo:
 dados_normalizado = normaliza(dados)
 pais_escolhido = sorteia_pais(dados_normalizado)
+info_escolhido = dados_normalizado[pais_escolhido]
+info_geo_escolhido = info_escolhido['geo']
+lat_escolhido = info_geo_escolhido['latitude']
+log_escolhido = info_geo_escolhido['longitude']
 
 while i < 20 :
-    palpite = str(input('Qual seu palpite:    '))
-    distancia_palpite = haversine(raio_terra, )
+    palpite = str(input('Qual seu palpite? :  '))
+    
+    info_palpite = dados_normalizado[palpite]
+    info_geo_palpite = info_palpite['geo']
+    lat_palpite = info_geo_palpite['latitude']
+    log_palpite = info_geo_palpite['longitude']
+    
+    distancia_palpite = haversine(raio_terra, lat_escolhido, log_escolhido, lat_palpite, log_palpite)
+    distancia_int = int(distancia_palpite)
+    print('Distancia: {}'.format(distancia_int))
+
+    
+    
+    
+    
+    
+    
     i += 1
 
 
