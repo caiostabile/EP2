318d1875b10531173a463321b97d92f768bf8b75
caiostabile <103457803+caiostabile@users.noreply.github.com>
2022-05-13 11:35:26 -0300

    Update junção_das_funções.py

--- Jogo/junção_das_funções.py
+++ Jogo/junção_das_funções.py
@@ -25,6 +25,30 @@ def haversine(raio_terra,l1,f1,l2,f2):
     return resposta
 
 ### Função 4
+def adiciona_em_ordem(nome_pais, distancia, lista_paises):
+
+    adicionar = [nome_pais,distancia]
+    i = 0
+    if len(lista_paises) > 0 :
+        tamanho = len(lista_paises)
+        ultimo_ele = lista_paises[tamanho-1]   
+        distancia_ultimo_elemnto = ultimo_ele[1]
+    if len(lista_paises) == 0:
+        resposta = lista_paises.append(adicionar)
+        
+    for sublista in lista_paises:
+        if sublista[0] == nome_pais:
+            return lista_paises
+            
+        if sublista[0] != nome_pais:
+            if sublista[1] >= distancia:
+                lista_paises.insert(i,adicionar)
+                return lista_paises
+            if distancia > distancia_ultimo_elemnto:
+                lista_paises.append(adicionar)
+                return lista_paises
+                        
+        i += 1
 
 ### Função 5
 
