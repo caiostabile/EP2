8f6fb4c69735bae5281c123f1dd33e95c38cfb57
caiostabile <103457803+caiostabile@users.noreply.github.com>
2022-05-13 11:33:05 -0300

    Update junção_das_funções.py

--- Jogo/junção_das_funções.py
+++ Jogo/junção_das_funções.py
@@ -17,6 +17,12 @@ def sorteia_pais(dados):
     return resultado
 
 ### Função 3
+import math
+def haversine(raio_terra,l1,f1,l2,f2):
+    parte1=(math.sin(math.radians((l2-l1)/2)))**2
+    parte2=math.cos(math.radians(l1))* math.cos(math.radians(l2))*(math.sin(math.radians((f2-f1)/2)))**2
+    resposta = 2*r*math.asin((parte1+parte2)**0.5)
+    return resposta
 
 ### Função 4
 
