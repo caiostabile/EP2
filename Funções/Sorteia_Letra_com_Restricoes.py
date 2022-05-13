c23e4507ab1433302089ff17530bccbab21adfd0
caiostabile <103457803+caiostabile@users.noreply.github.com>
2022-05-13 10:36:03 -0300

    Update Sorteia_Letra_com_Restricoes.py

--- Funções/Sorteia_Letra_com_Restricoes.py
+++ Funções/Sorteia_Letra_com_Restricoes.py
@@ -1,1 +1,9 @@
-def sorteia_letra(palavra, lista_restrita): 
+import random
+def sorteia_letra(palavra,lista_restrita):
+    letras_sorteio = []
+    caracteres_especiais = ['.', ',', '-', ';', ' ']
+    for letra in palavra:
+        if letra not in lista_restrita and letra not in caracteres_especiais:
+            letras_sorteio.append(letra)
+    resultado = random.choice(letras_sorteio)
+    return resultado
