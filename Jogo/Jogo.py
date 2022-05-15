5d820ffa20edbe26a9a8e8fd8dac9f860010c554
caiostabile <103457803+caiostabile@users.noreply.github.com>
2022-05-15 20:52:27 -0300

    Update Jogo.py

--- Jogo/Jogo.py
+++ Jogo/Jogo.py
@@ -9,21 +9,17 @@ while continua_jogo == True:
     from sklearn.decomposition import dict_learning
     from junção_das_funções import *
     from Dados import *
-    from colorama import Fore, Back, Style
 
 #variaveis:
     i = 0
     lista_paises = []
-    t = '\nDistância:\n'
+    lista_dicas = [0,1,2,3,4,5]
+    dicas_usadas = []
 
 #funções do jogo:
     dados_normalizado = normaliza(dados)
     pais_escolhido = sorteia_pais(dados_normalizado)
-
     info_escolhido = dados_normalizado[pais_escolhido]
-
-    info_escolhido = dados_normalizado[pais_escolhido]
-
     info_geo_escolhido = info_escolhido['geo']
     lat_escolhido = info_geo_escolhido['latitude']
     log_escolhido = info_geo_escolhido['longitude']
@@ -37,6 +33,7 @@ while continua_jogo == True:
         print("\n\nQuantidade de palpites: {}".format(20-i))
         palpite = str(input('Qual seu palpite? :  '))
         palpite = palpite.lower()
+        num_palpites = 20 - i 
     
     
         if palpite in lista_paises:
@@ -49,83 +46,102 @@ while continua_jogo == True:
 
             if palpite != pais_escolhido:
                 if i < 20:
-                    if 20 - i == 1:
+                    if num_palpites == 1:
                         i += 2
                     else:
-                        if distancia_int < 1000 :
-                            print(Fore.GREEN+'\nDistancia ao(a) {} é de {} km\n'.format(palpite,distancia_int)+Style.RESET_ALL)
-                        if distancia_int > 1000 and distancia_int <= 3000 :
-                            print(Fore.BLUE+'\nDistancia ao(a) {} é de {}\n'.format(palpite,distancia_int)+Style.RESET_ALL)
-                        if distancia_int > 3000 :
-                            print(Fore.RED+'\nDistancia ao(a) {} é de {}\n'.format(palpite,distancia_int)+Style.RESET_ALL)
+                        print('{} ------> {} Km'.format(palpite,distancia_int))
                         i += 1
             elif palpite == pais_escolhido:
-                print(Fore.GREEN+'\nPARABÉNS VOCÊ ACETOU!\n'+Style.RESET_ALL)
+                print("PARABÉNS VOCÊ ACETOU!")
                 continua_jogo = False
                 i = 22
         
+# DICAS
+            
+            dicas_usadas = []
 
         elif palpite == 'dica' or palpite =='Dica':
             print('MERCADO DE DICAS')
-            print('1 - cor da bandeira(valor: 4 palpites)\n2 - letra da capital(valor: 3 palpites)\n3 - área(valor: 6 palpites)\n4 - população(valor: 5 palpites)\n5 - continente(valor: 7 palpites)\n0 - sem dica(valor: 0 palpites)')
-            n_dica = int(input('Escolha sua opção [0 | 1 | 2 | 3 | 4 | 5]:'))
 
-            if n_dica == 1:
+            lista_dicas = []
+
+            lista_dicas.append(0)
+
+            if num_palpites > 4:
+                if 1 not in dicas_usadas:
+                    print('1 - cor da bandeira(valor: 4 palpites)')
+                    lista_dicas.append(1)
+            
+            if num_palpites > 3:
+                if 2 not in dicas_usadas:
+                    print('2 - letra da capital(valor: 3 palpites)')
+                    lista_dicas.append(2)
+            
+            if num_palpites > 6:
+                if 3 not in dicas_usadas:
+                    print('3 - área(valor: 6 palpites)')
+                    lista_dicas.append(3)
+
+            if num_palpites > 5:
+                if 4 not in dicas_usadas:
+                    print('4 - população(valor: 5 palpites)')
+                    lista_dicas.append(4)
+            
+            if num_palpites > 7:
+                if 5 not in dicas_usadas:
+                    print('5 - continente(valor: 7 palpites)')
+                    lista_dicas.append(5)
+
+
+            print('0 - sem dica(valor: 0 palpites)')
+
+            n_dica = int(input('Escolha sua opção {}:'.format(lista_dicas)))
+
+            
+
+            if n_dica == 1 and 1 in lista_dicas:
                 cores_bandeira = info_escolhido["bandeira"]
                 lista_cores = []
                 for cor,porcentagem in cores_bandeira.items():
                     cores_bandeira[cor] = porcentagem
                     if porcentagem >= 30:
                         lista_cores.append(cor)
-                if 20 - i > 4:
-                    print("Cores da bandeira {}". format(lista_cores))
-                    i += 4
-                elif 20 - i == 4:
-                    i += 5
-                else:
-                    print("Você nao tem palpites suficientes!")
+                
+                print("Cores da bandeira {}". format(lista_cores))
+                i += 4
+                dicas_usadas.append(1)
+               
 
-            elif n_dica == 2:
+            elif n_dica == 2 and 2 in lista_dicas:
                 capital = info_escolhido["capital"]
-                if 20 - i > 3:
-                    print("Uma letra da capital: {}". format(sorteia_letra(palpite,[capital[0]])))
-                    i += 3
-                elif 20 - i == 3:
-                    i += 4
-                else: 
-                    print("Você nao tem palpites suficientes!")
-            elif n_dica == 3:
-                if 20 - i > 6:
-                    print("Área: {} Km".format(info_escolhido["area"]))
-                    i += 6
-                elif 20 - i == 6:
-                    i += 7
-                else: 
-                    print("Você nao tem palpites suficientes!")
+                print("Uma letra da capital: {}". format(sorteia_letra(palpite,[capital[0]])))
+                i += 3
+                dicas_usadas.append(2)
 
-            elif n_dica == 4:
-                if 20 - i > 5:
-                    print("População: {} habitantes".format(info_escolhido["populacao"]))
-                    i += 5
-                elif 20 - i == 5:
-                    i += 6
-                else: 
-                    print("Você nao tem palpites suficientes!")
 
-            elif n_dica == 5:
-                if 20 - i > 7:
-                    print("Continente: {}".format(info_escolhido["continente"]))
-                    i += 7
-                elif 20 - i == 7:
-                    i += 8
-                else: 
-                    print("Você nao tem palpites suficientes!")
-            
-        else :
+            elif n_dica == 3 and 3 in lista_dicas:
+                print("Área: {} Km".format(info_escolhido["area"]))
+                i += 6
+                dicas_usadas.append(3)
+                
+            elif n_dica == 4 and 4 in lista_dicas:
+                print("População: {} habitantes".format(info_escolhido["populacao"]))
+                i += 5
+                dicas_usadas.append(4)
+
+            elif n_dica == 5 and 5 in lista_dicas:
+                print("Continente: {}".format(info_escolhido["continente"]))
+                i += 7
+                dicas_usadas.append(5)
+ 
+            elif len(lista_dicas) == 1:
+                print("Não é possível acessar o mercado de dicas, pois seu estoque de palpites é insuficiente")
+
+        elif palpite not in lista_paises :
             print('país desconhecido')  
 
     if i == 21:
-        print(Fore.RED+"\nVOCÊ PERDEU!\nO país era {}\n".format(pais_escolhido)+Style.RESET_ALL)
+        print("VOCÊ PERDEU!\nO país era {}".format(pais_escolhido))
         i = 22
 
     if i == 22:
@@ -134,5 +150,4 @@ while continua_jogo == True:
             continua_jogo = True
             i = 0
         if continua_jogo == 'n':
-
-            continua_jogo = False
+            continua_jogo = False                
